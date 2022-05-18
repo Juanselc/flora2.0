565f31036f4016baa92b4b10cf45b0f8f77a2db9
Juanselc <leoncjuan@gmail.com>
2022-05-17 21:44:52 -0500

    dos

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'flora2-69c1f',
+    appId: '1:790217845050:web:6b48b18e8fa7ae26484803',
+    storageBucket: 'flora2-69c1f.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyCr_8FNLU__QVj9WOYkosq_pXtKLf7j7L8',
+    authDomain: 'flora2-69c1f.firebaseapp.com',
+    messagingSenderId: '790217845050',
+  },
+  production: true
+};
